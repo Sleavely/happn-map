808fa1fa2f0a22de7eb04a46c5d67bebd76d5c75
Joakim Hedlund <contact@joakimhedlund.com>
2018-04-29 21:01:01 +0200

    Feeble attempt at making it comprehensible

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -9,53 +9,13 @@
   }
 })(async function(){
 
-  const apiResponse = await fetch('/api/v1/timeline', {
-    cache: 'no-cache',
-    credentials: 'same-origin', // <- make fetch() include cookies in the req
-    headers: {'accept': 'application/json','content-type': 'application/json'}
-  })
-  .then((response) => response.json())
-  .catch((err) => console.error(err))
+  const hm = new HappnMap;
+  let dataset = await hm.init()
 
-  if(apiResponse.error) return console.error(apiResponse.error.message, apiResponse.error)
 
-  const dataset = new vis.DataSet(apiResponse.map((encounter, i) => {
-    encounter.start = new Date(encounter.last)
-    return encounter
-  }))
-
-  // DOM element where the Timeline will be attached
-  const visContainer = document.getElementById('visualization')
-
-  // Configuration for the Timeline
-  const timelineOptions = {
-    template: (encounter, element, data) => {
-      element.classList.add('photo')
-      element.style.backgroundImage = `url(${encounter.photo})`
-
-      return `<div>
-        <h3><a href="https://www.google.com/maps/place/${encounter.lat},${encounter.lng}" target="_blank">${encounter.name}</a></h3>
-      </div>`
-    },
-    height: '400px',
-    width: '100%',
-    /**
-     * The zooming is weird in that it sets the min/max amount of time you can
-     * see ACROSS the screen, rather than the unit of time used in the grid.
-     * For responsive websites this means smaller screens will have trouble zooming.
-     */
-    zoomMax: (1000*60*60*24*7),
-    zoomMin: (1000*60)
-  };
-
-  // Create a Timeline
-  const timeline = new vis.Timeline(visContainer, dataset, timelineOptions);
-
-  // Default to latest 24h
-  timeline.setWindow((new Date)-86400000, (new Date))
 
   //TODO: change the size of items based on how far zoomed we are?
-  timeline.on('rangechanged', function (e) {
+  hm.timeline.on('rangechanged', function (e) {
     // Only react to changes made by the user
     if(e.byUser)
     {
@@ -65,3 +25,114 @@
     //TODO: this should trigger a repaint in gmaps
   });
 })
+
+class HappnMap {
+  constructor(options)
+  {
+    const defaults = {
+      recommendationsLimit: 16,
+      recommendationsOffset: 0,
+      timelineElementId: 'visualization',
+      timelineWidth: '100%',
+      timelineHeight: '400px'
+    }
+    this.options = Object.assign({}, defaults, options || {})
+  }
+
+  async init()
+  {
+    await this.initDataset()
+    await this.initTimeline()
+    await this.initMap()
+    this.syncMap()
+  }
+
+  async initDataset()
+  {
+    this.dataset = new vis.DataSet()
+    await this.getEncounters()
+  }
+
+  async initTimeline()
+  {
+    this.timelineOptions = {
+      template: this.renderTimelineItem,
+      width: this.options.timelineWidth,
+      height: this.options.timelineHeight,
+      // The zooming is weird in that it sets the min/max amount of time you can
+      // see ACROSS the screen, rather than the unit of time used in the grid.
+      // For responsive websites this means smaller screens will have trouble zooming.
+      zoomMax: (1000*60*60*24*7),
+      zoomMin: (1000*60)
+    };
+    this.visContainer = document.getElementById(this.options.timelineElementId)
+    this.timeline = new vis.Timeline(this.visContainer, this.dataset, this.timelineOptions)
+    // Default to latest 24h
+    this.timeline.setWindow((new Date)-86400000, (new Date))
+  }
+
+  async initMap()
+  {
+
+  }
+
+  async fetch(uri, opts)
+  {
+    const defaultOpts = {
+      cache: 'no-cache',
+      credentials: 'same-origin', // <- make fetch() include cookies in the req
+      headers: {'accept': 'application/json','content-type': 'application/json'}
+    }
+    const finalOpts = Object.assign({}, defaultOpts, opts || {})
+    const apiResponse = await fetch(uri, finalOpts)
+      .then((response) => response.json())
+      .catch((err) => console.error(err))
+    if(apiResponse.error) throw new Error(apiResponse.error)
+    return apiResponse
+  }
+
+  /**
+   * Request encounters from the API.
+   *
+   * @param {Number} limit
+   * @param {Number} offset
+   * @returns {Array}
+   */
+  async getEncounters(limit, offset)
+  {
+    if(!limit) limit = this.options.recommendationsLimit
+    if(!offset) offset = this.options.recommendationsOffset
+
+    const response = await this.fetch(`/api/v1/timeline?limit=${limit}&offset=${offset}`)
+
+    // Add to the dataset
+    const formattedData = response.map((encounter, i) => {
+      encounter.start = new Date(encounter.last)
+      delete encounter.last
+      return encounter
+    })
+
+    //TODO: we should make sure we arent adding duplicates
+    this.dataset.add(formattedData)
+    return this.dataset
+  }
+
+  renderTimelineItem(encounter, element, data)
+  {
+    element.classList.add('photo')
+    element.style.backgroundImage = `url(${encounter.photo})`
+
+    return `<div>
+      <h3><a href="https://www.google.com/maps/place/${encounter.lat},${encounter.lng}" target="_blank">${encounter.name}</a></h3>
+    </div>`
+  }
+
+  /**
+   * Add any items that arent already on the map, fit map to the markers in the range
+   */
+  syncMap()
+  {
+    //
+    //TODO: get visible items
+  }
+}
